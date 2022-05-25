defe52bfd4e67c71a3d844564b304cc9c7ff4288
Denis <denis_1293@hotmail.com>
2022-05-25 21:54:01 +0300

    huita

---
+++ ProjectOne/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert("hello");
+
+// const answer = prompt('why are you gay?');
+
+// console.log(answer);
+
+const answers = [];
+
+answers[0]= prompt("ты че?",'');
+answers[1]= prompt("ты че ска?",'');
+answers[2]= prompt("ты че бля?",'');
+document.write(answers);
